5c43fb377fafa62fe7a8aa67eb872a68ee5471bf
chingweichan <willy756.taiwan@gmail.com>
2023-08-19 21:19:15 +0800

    新增討論區

--- pages/items.py
+++ pages/items.py
@@ -13,14 +13,43 @@ for tab, label in zip(tabs, st.session_state["label"]):
         question_with_label = [
             x for x in st.session_state["questions"] if label in x[2]]
         for questions in question_with_label:
+            tmp = next(counter)
             # st.subheader("題目")
             st.code(f"{questions[0]}")
             with st.expander("答案"):
                 st.code(f"{questions[1]}")
             st.write(f"""標籤：{", ".join(questions[2])}""")
+            with st.expander("討論區"):
+                get_discussion = run_query(
+                    f"""SELECT discussion from questions WHERE question = '{questions[0]}';""")
+                if get_discussion[0][0]:
+                    for discussion in get_discussion[0][0]:
+                        st.code(discussion[1:-1])
+
+                st.divider()
+                with st.form(key=f"form_{tmp}"):
+                    new_user = st.text_input(
+                        "討論者名稱：", key=f"user_{tmp}")
+                    new_discussion = st.text_area(
+                        "討論內容：", key=f"new_discussion_{tmp}")
+                    submitted = st.form_submit_button(
+                        "加入討論吧！")
+                    if submitted:
+                        if get_discussion == [(None,)]:
+                            content = [f"({new_user}: {new_discussion})"]
+                        else:
+                            content = get_discussion[0][0] + \
+                                [f"({new_user}: {new_discussion})"]
+                        run_query(
+                            f"""UPDATE questions SET \
+                                discussion=ARRAY{content} \
+                            WHERE question='{questions[0]}';""")
+                        st.write("加入成功！")
+
             col1, col2 = st.columns([0.1, 0.9])
+
             st.divider()
-            if col1.button("刪除題目", key=f"D_{(tmp:=next(counter))}"):
+            if col1.button("刪除題目", key=f"D_{tmp}"):
                 run_query(
                     f"DELETE FROM questions WHERE question = '{questions[0]}';")
                 st.experimental_rerun()
